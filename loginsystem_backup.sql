f0ba66b6074481a5cf60f664878ec9a811ac56d6
Koala <johnpa9191@gmail.com>
2024-06-11 12:11:50 +0800

    update 2024/06/11

---
+++ loginsystem_backup.sql
@@ -0,0 +1,58 @@
+-- MariaDB dump 10.19  Distrib 10.11.7-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: loginsystem
+-- ------------------------------------------------------
+-- Server version	10.11.7-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `login_inf`
+--
+
+DROP TABLE IF EXISTS `login_inf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login_inf` (
+  `user_id` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `last_ip` varchar(50) DEFAULT NULL,
+  `status` char(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `last_ip` (`last_ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login_inf`
+--
+
+LOCK TABLES `login_inf` WRITE;
+/*!40000 ALTER TABLE `login_inf` DISABLE KEYS */;
+INSERT INTO `login_inf` VALUES
+('','',NULL,'0'),
+('test2','012345',NULL,'0'),
+('testing','012345',NULL,'0'),
+('WU41043117','WU20021111','::1','0');
+/*!40000 ALTER TABLE `login_inf` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-11 12:10:55
